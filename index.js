ad9bcf7f02a762886baff2768ebab73a0015d94f
Kaolhou <xdroid42@gmail.com>
2022-01-13 19:31:05 -0300

    express init

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("oi")
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/',(req, res)=>{
+    res.send('bababoi?')
+})
+
+app.listen(port,()=>{
+    console.log(`server started at http://localhost:${port}`)
+})
