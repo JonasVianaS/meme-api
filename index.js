310cc52aa0511f64d83e088df64baa0200b14f83
Kaolhou <xdroid42@gmail.com>
2022-01-13 21:14:47 -0300

    código de base

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const port = 5000
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
 app.get('/',(req, res)=>{
-    res.send('bababoi?')
+    res.sendFile(__dirname+'/pages/home/index.html')
+})
+
+app.post('/hehe',(req,res)=>{
+    res.send(process.env.um)
 })
 
 app.listen(port,()=>{
