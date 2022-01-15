4eb86da81e368420f6ff9bf9693900c68fcfa4d0
Kaolhou <xdroid42@gmail.com>
2022-01-14 21:26:25 -0300

    sendFile

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const express = require('express')
 const app = express()
-require('dotenv').config()
+const upload = require('express-fileupload')
 const port = 5000
-
+const formats = require('./modules/formats')
+require('dotenv').config()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
+app.use(upload())
 
 app.get('/',(req, res)=>{
     res.sendFile(__dirname+'/pages/home/index.html')
 })
 
-app.post('/hehe',(req,res)=>{
-    res.send(process.env.um)
+app.get('/addmanager',(req, res)=>{
+    res.sendFile(__dirname+'/pages/post/index.html')
+})
+
+app.post('/add',(req,res)=>{
+    const fileName = req.files.file.name
+    const file = req.files.file
+    const x = fileName.split('.')
+    // image add
+    if(formats.image.filter((i)=>{return i === x[x.length-1]}).toString()){
+        console.log('image')
+        file.mv('./assets/images/'+fileName,function(err){
+            err?res.send(err):res.send("image uploaded")
+        })
+    }
+    // video add
+    if(formats.video.filter((i)=>{return i === x[x.length-1]}).toString()){
+        console.log('video')
+        file.mv('./assets/videos/'+fileName,function(err){
+            err?res.send(err):res.send("video uploaded")
+        })
+    }
 })
 
 app.listen(port,()=>{
---
+++ modules/formats.js
@@ -0,0 +1,16 @@
+const format = {
+    video: [
+        "mp4",
+        "ogg",
+        "mkv",
+        "mpeg4",
+        "mov",
+        "webm"
+    ],
+    image: [
+        "jpg",
+        "jpeg",
+        "png"
+    ]
+}
+module.exports = format
